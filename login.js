51c07a25df3dd7a7e17addffbe174b9d09b26711
varun3003 <varunpradeep30@gmail.com>
2024-07-12 12:59:49 +0530

    login logic

---
+++ login.js
@@ -0,0 +1,36 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent the form from submitting the default way
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const loginData = {
+        username: username,
+        password: password
+    };
+
+    try {
+        const response = await fetch('https://dummyjson.com/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        });
+
+        const data = await response.json();
+
+        if (data.token) {
+            console.log('Login successful', data);
+            // Save the response data to localStorage
+            localStorage.setItem('userDetails', JSON.stringify(data));
+            // Redirect to another page
+            window.location.href = 'home.html';
+        } else {
+            console.log('Login failed', data);
+            // Handle login failure
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
